8b0c1d1a32f8d449e0a283b818b9ef5baac67323
DmitryK777 <kislyakovdmv@gmail.com>
2024-12-01 15:44:56 +0300

    Calc Buttons DONE

--- WinAPI/Calc/main.cpp
+++ WinAPI/Calc/main.cpp
@@ -27,19 +27,33 @@
 
 CONST CHAR g_sz_WINDOW_CLASS[] = "Calc_VPD_311";
 
-CONST INT g_i_SCREEN_WIDTH = 400;
-CONST INT g_i_SCREEN_HEIGHT = 22;
 
 CONST INT g_i_BUTTON_SIZE = 50; // Размер кнопки в пикселах
 CONST INT g_i_INTERVAL = 5;     // Расстояние между кнопками
+CONST INT g_i_BUTTON_DOUBLE_SIZE = g_i_BUTTON_SIZE * 2 + g_i_INTERVAL; // Размер большой кнопки в пикселах
+
+CONST INT g_i_SCREEN_WIDTH = g_i_BUTTON_SIZE * 5 + g_i_INTERVAL * 4;
+CONST INT g_i_SCREEN_HEIGHT = 22;
 
 CONST INT g_i_START_X = 10;     // 
 CONST INT g_i_START_Y = 10;     // 
 CONST INT g_i_BUTTON_START_X = g_i_START_X;     // 
 CONST INT g_i_BUTTON_START_Y = g_i_START_Y + g_i_SCREEN_HEIGHT + g_i_INTERVAL;     // 
 
+#define BUTTON_SHIFT_X(column) g_i_BUTTON_START_X + (g_i_BUTTON_SIZE + g_i_INTERVAL) *(column)
+#define BUTTON_SHIFT_Y(row) g_i_BUTTON_START_Y + (g_i_BUTTON_SIZE + g_i_INTERVAL) *(row)
+
+
+CONST INT g_i_WINDOW_WIDTH = g_i_SCREEN_WIDTH + g_i_START_X *2 + 8 + 8;
+CONST INT g_i_WINDOW_HEIGHT = 43 + g_i_START_X + g_i_SCREEN_HEIGHT + g_i_BUTTON_SIZE * 4 + g_i_INTERVAL * 5;
+
+
+CONST CHAR* g_OPERATIONS[] = { "+", "-", "*", "/" };
+
 
 INT CALLBACK WndProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
+
+INT GetTitleBarHeight(HWND hwnd);
 
 INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInst, LPSTR lpCmdLine, INT nCmdShow)
 {
@@ -59,7 +73,7 @@ INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInst, LPSTR lpCmdLine, IN
 
 	wClass.hInstance = hInstance;
 	wClass.lpszClassName = g_sz_WINDOW_CLASS;
-	wClass.lpszMenuName = g_sz_WINDOW_CLASS;
+	wClass.lpszMenuName = NULL;
 	wClass.lpfnWndProc = (WNDPROC)WndProc;
 
 	if (!RegisterClassEx(&wClass))
@@ -71,14 +85,21 @@ INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInst, LPSTR lpCmdLine, IN
 
 	// 2) Создание окна
 
+	INT screen_width = GetSystemMetrics(SM_CXSCREEN);
+	INT screen_height = GetSystemMetrics(SM_CYSCREEN);
+
+	INT window_start_x = (screen_width / 2) - (g_i_WINDOW_WIDTH / 2);
+	INT window_start_y = (screen_height / 2) - (g_i_WINDOW_HEIGHT);
+
+
 	HWND hwnd = CreateWindowEx
 	(
 		NULL,
 		g_sz_WINDOW_CLASS,
 		g_sz_WINDOW_CLASS,
-		WS_OVERLAPPEDWINDOW,
-		CW_USEDEFAULT, CW_USEDEFAULT,
-		CW_USEDEFAULT, CW_USEDEFAULT,
+		WS_OVERLAPPEDWINDOW ^ WS_THICKFRAME ^ WS_MAXIMIZEBOX,
+		window_start_x, window_start_y,
+		g_i_WINDOW_WIDTH, g_i_WINDOW_HEIGHT,
 		NULL,
 		NULL,
 		hInstance,
@@ -111,15 +132,113 @@ INT CALLBACK WndProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 				(
 					NULL, "Edit", "0",
 					WS_CHILD | WS_VISIBLE | WS_BORDER | ES_RIGHT,
-					10, 10,
-					400, 22,
+					g_i_START_X, g_i_START_Y,
+					g_i_SCREEN_WIDTH, g_i_SCREEN_HEIGHT,
 					hwnd,
-					(HMENU)1000,
+					(HMENU)999,
 					GetModuleHandle(NULL),
 					NULL
 				);
+
+				CHAR sz_digit[2]{};
+				for (int i = 6; i >= 0; i -= 3)
+				{
+					for (int j = 0; j < 3; j++)
+					{
+						sz_digit[0] = i + j + '1';
+						CreateWindowEx
+						(
+							NULL, "Button", sz_digit,
+							WS_CHILD | WS_VISIBLE,
+							g_i_BUTTON_START_X + (g_i_BUTTON_SIZE + g_i_INTERVAL) * j,
+							g_i_BUTTON_START_Y + (g_i_BUTTON_SIZE + g_i_INTERVAL) * (2 - i/3),
+							g_i_BUTTON_SIZE, g_i_BUTTON_SIZE,
+							hwnd,
+							(HMENU)IDC_BUTTON_1 + (i + j),
+							GetModuleHandle(NULL),
+							NULL
+						);
+					}
+
+					CreateWindowEx
+					(
+						NULL, "Button", "0",
+						WS_CHILD | WS_VISIBLE,
+						BUTTON_SHIFT_X(0), BUTTON_SHIFT_Y(3),
+						g_i_BUTTON_DOUBLE_SIZE, g_i_BUTTON_SIZE,
+						hwnd,
+						(HMENU)IDC_BUTTON_0,
+						GetModuleHandle(NULL),
+						NULL
+					);
+				}
+
+				CreateWindowEx
+				(
+					NULL, "Button", ",",
+					WS_CHILD | WS_VISIBLE,
+					BUTTON_SHIFT_X(2), BUTTON_SHIFT_Y(3),
+					g_i_BUTTON_SIZE, g_i_BUTTON_SIZE,
+					hwnd,
+					(HMENU)IDC_BUTTON_POINT,
+					GetModuleHandle(NULL),
+					NULL
+				);
+
+				for (int i = 0; i < 4; i++)
+				{
+					CreateWindowEx
+					(
+						NULL, "Button", g_OPERATIONS[i],
+						WS_CHILD | WS_VISIBLE,
+						BUTTON_SHIFT_X(3), BUTTON_SHIFT_Y(3-i),
+						g_i_BUTTON_SIZE, g_i_BUTTON_SIZE,
+						hwnd,
+						(HMENU)IDC_BUTTON_PLUS + i,
+						GetModuleHandle(NULL),
+						NULL
+					);
+				}
+
+				CreateWindowEx
+				(
+					NULL, "Button", "<-",
+					WS_CHILD | WS_VISIBLE,
+					BUTTON_SHIFT_X(4), BUTTON_SHIFT_Y(0),
+					g_i_BUTTON_SIZE, g_i_BUTTON_SIZE,
+					hwnd,
+					(HMENU)IDC_BUTTON_BSP,
+					GetModuleHandle(NULL),
+					NULL
+				);
+
+				CreateWindowEx
+				(
+					NULL, "Button", "C",
+					WS_CHILD | WS_VISIBLE,
+					BUTTON_SHIFT_X(4), BUTTON_SHIFT_Y(1),
+					g_i_BUTTON_SIZE, g_i_BUTTON_SIZE,
+					hwnd,
+					(HMENU)IDC_BUTTON_CLR,
+					GetModuleHandle(NULL),
+					NULL
+				);
+
+				CreateWindowEx
+				(
+					NULL, "Button", "=",
+					WS_CHILD | WS_VISIBLE,
+					BUTTON_SHIFT_X(4), BUTTON_SHIFT_Y(2),
+					g_i_BUTTON_SIZE, g_i_BUTTON_DOUBLE_SIZE,
+					hwnd,
+					(HMENU)IDC_BUTTON_EQUAL,
+					GetModuleHandle(NULL),
+					NULL
+				);
+			
 			}
 			break;
+
 		case WM_COMMAND:
 			{
 
@@ -132,4 +251,17 @@ INT CALLBACK WndProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 	}
 	
 	return FALSE;
+}
+
+INT GetTitleBarHeight(HWND hwnd)
+{
+	RECT window_rect;
+	RECT client_rect;
+
+	GetWindowRect(hwnd, &window_rect);
+	GetWindowRect(hwnd, &client_rect);
+
+	INT title_bar_height = (window_rect.bottom - window_rect.top) - (client_rect.bottom - client_rect.top);
+
+	return title_bar_height;
 }
