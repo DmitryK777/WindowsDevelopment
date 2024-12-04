2bba6531e67263a8768c3d4890ca755cab9aad0a
DmitryK777 <kislyakovdmv@gmail.com>
2024-12-04 12:56:08 +0300

    Changes in IDC_BUTTONS

--- WinAPI/Calc/resource.h
+++ WinAPI/Calc/resource.h
@@ -19,10 +19,10 @@
 #define IDC_BUTTON_9        1009
 #define IDC_BUTTON_POINT    1010
 
-#define IDC_BUTTON_PLUS     1010
-#define IDC_BUTTON_MINUS    1010
-#define IDC_BUTTON_ASTER    1010 // "*"
-#define IDC_BUTTON_SLASH    1010 // '/'
+#define IDC_BUTTON_PLUS     1011
+#define IDC_BUTTON_MINUS    1012
+#define IDC_BUTTON_ASTER    1013 // "*"
+#define IDC_BUTTON_SLASH    1014 // '/'
 
 #define IDC_BUTTON_BSP      1015 // BackSpace
 #define IDC_BUTTON_CLR      1016 // Clear
